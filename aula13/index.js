68c9ac7fc014ba8c7d792a15ee7d8812de76239f
Calebeer <sostenesapollo55@gmail.com>
2023-01-30 19:45:29 -0300

    att

--- aula13/index.js
+++ aula13/index.js
@@ -2,7 +2,7 @@
 //  let umaInstring = "fala aí"
 //  console.log(umaInstring[2])
 
-let umaInstring = "Um texto"
+let umaInstring = "Um Texto"
 // console.log(umaInstring.concat(' em um lindo dia'))
 // console.log(umaInstring + ' em um lindo dia')
 // console.log(`${umaInstring} em um lindo dia`)
@@ -15,6 +15,12 @@ let umaInstring = "Um texto"
 
 // console.log(umaInstring.length) // Para ver o tamanho da string
 
-console.log(umaInstring.slice(3,8)) // para escolher onde e ele vai começar e onde vai terminar
+// console.log(umaInstring.slice(3,8)) // para escolher onde e ele vai começar e onde vai terminar
+
+// console.log(umaInstring.split(' ', 2));// Para separar a frase com os espaços
+
+// console.log(umaInstring.toUpperCase()) // Para deixar as letras maiusculas.
+
+console.log(umaInstring.toLowerCase()) // para deixar as palavras minusculas
 
 
