d380422dde2d5ecdc1b4d876b2f179451231c6cb
Calebeer <sostenesapollo55@gmail.com>
2023-01-31 22:49:46 -0300

    att

--- aula15/ex.js
+++ aula15/ex.js
@@ -3,13 +3,21 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Raiz quadrada: ${Math.sqrt(numero)}  </p>;
-    <p>${numero} é inteiro: ${Number.isInteger(numero)}</p>
-    <p>É NaN: ${isNaN(numero)} </p>
-    <p>Arredondando para baixo: ${Math.floor(numero)}</p>
-    <p>Arredondando para cima: ${Math.ceil(numero)}</p>
-    <p>Com duas casas decimais: `;
+// texto.innerHTML = `<p>Raiz quadrada: ${Math.sqrt(numero)}  </p>
+//     <p>${numero} é inteiro: ${Number.isInteger(numero)}</p>
+//     <p>É NaN: ${isNaN(numero)}</p>
+//     <p>Arredondando para baixo: ${Math.floor(numero)} </p>
+//     <p>Arredondando para cima: ${Math.ceil(numero)}</p>
+//     <p> Com duas casas decimais: ${numero.toFixed(2)}`;
 
+                 // OU
+
+texto.innerHTML += `<p>Raiz quadrada: ${Math.sqrt(numero)}  </p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)} </p>`;
+texto.innerHTML += `<p>É NaN: ${isNaN(numero)}</p>`;
+texto.innerHTML +=  `<p>Arredondando para baixo: ${Math.floor(numero)} </p>`;
+texto.innerHTML +=  ` <p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p> Com duas casas decimais: ${numero.toFixed(2)}`;
 
 
 
