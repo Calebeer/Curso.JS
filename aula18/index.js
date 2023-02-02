83148e6adb83280e52b16fbc5bd954e50edc2ab8
Calebeer <sostenesapollo55@gmail.com>
2023-02-01 22:13:44 -0300

    att

---
+++ aula18/index.js
@@ -0,0 +1,36 @@
+// const raiz = function (n){
+//     return n ** 0.5
+// };
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(16))
+
+
+
+
+
+// function soma(x,y){
+//     const resultado = x+y;
+//     return resultado;
+// }
+
+// const resultado = soma(2,6);
+// console.log(resultado)
+
+
+
+//function soma (x,y){
+  //  const resultado = x+y;
+    //return resultado
+//}
+
+//console.log(soma(2, 2)); 
+
+
+//function saudacao(nome){
+  //  return (`Bom dia ${nome}`);
+//}
+
+//const variavel = saudacao('Calebe')
+//console.log(variavel)
