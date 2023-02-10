b417437151e40b7d144c5589430c96f7a117fc10
Calebeer <sostenesapollo55@gmail.com>
2023-02-10 13:08:44 -0300

    att

--- aula19/index.js
+++ aula19/index.js
@@ -56,3 +56,39 @@ console.log(pessoal.idade)
 // array.push(4);
 // array[0] = 'Calebe'
 // console.log(array)
+
+
+
+
+
+function meuEscop(){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
+
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+
+        const pessoas = [];
+
+        const nome =  form.querySelector('.nome');
+        const sobrenome =  form.querySelector('.sobrenome');
+        const peso =  form.querySelector('.peso');
+        const altura =  form.querySelector('.altura');
+
+        pessoas.push({
+            nome:nome.value,
+            sobrenome:sobrenome.value,
+            peso : peso.value,
+            altura : altura.value
+        });
+        
+        console.log(pessoas);
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+
+    }
+    
+    form.addEventListener('submit', recebeEventoForm);
+}
+
+
+meuEscop()
--- tests/index.js
+++ tests/index.js
@@ -1,31 +1,28 @@
-function meuEscop(){
+function meuEscope(){
     const form = document.querySelector('.form');
-    const resultado = document.querySelector('.resultado')
+    const resultado = document.querySelector('.resultado');
 
-    function recebeEventoForm (evento){
+    function recebeEventoForm(evento){
         evento.preventDefault();
 
         const pessoas = [];
 
-        const nome =  form.querySelector('.nome');
-        const sobrenome =  form.querySelector('.sobrenome');
-        const peso =  form.querySelector('.peso');
-        const altura =  form.querySelector('.altura');
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura')
 
         pessoas.push({
             nome:nome.value,
             sobrenome:sobrenome.value,
-            peso : peso.value,
-            altura : altura.value
-        });
-        
-        console.log(pessoas);
-        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+            peso:peso.value,
+            altura:altura.value
+        })
+        console.log(pessoas)
 
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>`
     }
-    
-    form.addEventListener('submit', recebeEventoForm);
+    form.addEventListener('submit', recebeEventoForm)
 }
 
-
-meuEscop()
+meuEscope()
