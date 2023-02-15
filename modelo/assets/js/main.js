323ec79f05db538d4e4e8ea73878604ce019e7a8
Calebeer <sostenesapollo55@gmail.com>
2023-02-14 23:48:35 -0300

    meu final

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -1,12 +1,12 @@
 function calculoImc(){
     const cadastros = [];
-
+    
     function recebeCalculo(evento){
         evento.preventDefault()
         const peso = document.querySelector('#peso').value;
         const altura = document.querySelector('#altura').value;
         const result = document.querySelector('.resultado');
-        
+        const erro = document.querySelector('.erro')
         
         cadastros.push({
             peso:parseFloat(peso),
@@ -16,18 +16,20 @@ function calculoImc(){
         console.log(cadastros);
         const resultado = (parseFloat(peso /(altura *altura))).toFixed(2);
         
-        if (resultado == 0 && resultado == 18,5 ){
+        if (resultado == 0 && resultado == 18.5 ){
            nome = "(Abaixo do peso)"
-        }else if (resultado >= 18,5 && resultado <= 24,99 ){
+        }else if (resultado >= 18.5 && resultado <= 24.99 ){
             nome = "(Peso normal)"
-        }else if (resultado >= 25 && resultado <= 29,9){
+        }else if (resultado >= 25 && resultado <= 29.9){
             nome = "(Sobrepeso)"
-        }else if(resultado >= 30 && resultado <= 34,9){
+        }else if(resultado >= 30 && resultado <= 34.9){
             nome = "(Obesidade grau 1)"
-        }else if (resultado >= 35 && 39,9){
+        }else if (resultado >= 35 && 39.9){
             nome = "(Obesidade grau 2)"
+        }else if(document.querySelector('#peso').value === ""  && document.querySelector('#altura').value === ""){
+            erro.innerHTML = `Peso inválido`
         }else{
-            nome = "(Obesidade grau 3)"
+            console.log('nada')
         }
          
 
